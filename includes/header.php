e137b91bbd1d8710f6684a8388fedd320612db49
cquentin26200 <77721932+cquentin26200@users.noreply.github.com>
2022-11-21 20:36:55 +0100

    add header

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?? "Pizza Vincent" ?></title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="scss/style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="groupHeader">
+                <a href="#"><img src="https://s4z4b4w5.stackpathcdn.com/pm/wp/vincent/wp-content/themes/vincent/img/logo.png" alt=""></a>
+                <div>
+                    <span class="activeMenu"></span>
+                </div>
+            </div>
+        </nav>
+    </header>
